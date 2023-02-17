f14bb7e9da34122470a29552a53515e386b48443
Yudi Pratama, I Gnb <ibagus.yudi@gmail.com>
2023-02-17 16:43:39 +0800

    ci: add github workflow and procfile

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import cookieParser from 'cookie-parser';
 import express, { Express } from 'express';
+import cors, { CorsOptions } from 'cors';
 
 const app: Express = express();
 
+const corsOptions: CorsOptions = {
+  origin: ['http://localhost:3000'],
+  credentials: true,
+};
+
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 export default app;
